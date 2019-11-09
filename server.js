7ea80bffdec68e903e8a96c446aa148471089f1f
Keith Francis Balde <kbalde@wayfair.com>
2019-11-09 11:39:10 -0500

    setup db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const db = require('./backend/db/db');
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
 const port = process.env.PORT || 3000;
 
@@ -12,4 +13,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-
+app.get('/users', (req, res) => {
+  if(Object.values(req.query).length < 1) {
+    db.getAllUsers()
+      .then(data => res.send(data))
+      .catch(error => console.log(error));
+  } else {
+      if(req.query.email) {
+        db.getEmail(req.query.email)
+          .then(data => res.send(data))
+          .catch(error => console.log(error));
+      }
+    }
+});
