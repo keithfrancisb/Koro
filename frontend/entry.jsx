7e1b2776142498edd3040f78078ddf75ca600c6e
Keith Francis Balde <kbalde@wayfair.com>
2019-11-09 11:55:44 -0500

    setup root

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 
 // When ever the document has been loaded it executes everything inside the call back
 document.addEventListener('DOMContentLoaded', () =>
 {
   const root = document.getElementById('root');
-  ReactDOM.render("esfaefsesfefesfe", root);
+  ReactDOM.render(<Root />, root);
 });
