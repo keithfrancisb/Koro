bdb4eaeaa75fe40863501e1dd56b44b648efd2e6
Keith Francis Balde <kbalde@wayfair.com>
2019-11-09 12:36:04 -0500

    setup initial login

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import './Login.css';
 
 export default class Login extends React.Component{
