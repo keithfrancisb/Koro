25eece4b6304bb0dd25481d325d307357ba98496
Keith Francis Balde <kbalde@wayfair.com>
2019-11-09 15:55:19 -0500

    add container in app

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -2,16 +2,25 @@ import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 import Login from './Login';
 import {signup} from '../util/services';
+import Container from 'react-bootstrap/Container';
+class App extends React.Component {
+  constructor() {
+    super();
 
-const App = () => {
-  return (
-    <div>
-      I am the App!
+    this.state = {
+      email: null,
+    };
+  }
 
-      <Route path='/' render={() => <Redirect to='/signup'/>}/>
-      <Route path='/signup' render={() => <Login handleSubmit={signup}/>}/>
-    </div>
-  );
+  render() {
+    return (
+      <Container>
+
+        <Route path='/' render={() => <Redirect to='/signup'/>}/>
+        <Route path='/signup' render={() => <Login handleSubmit={signup}/>}/>
+      </Container>
+    );
+  }
 };
 
 export default App;
