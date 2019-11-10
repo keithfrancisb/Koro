3618eced56e586394616ef79cf87f34211f2d1ab
BryanJusto <tsarbelyyvolk@gmail.com>
2019-11-09 20:16:22 -0500

    >added some touches

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,14 @@
 const pgp = require('pg-promise')();
+<<<<<<< HEAD
 const db = pgp('SQLKORA://postgres:123@localhost:3001/kora');
 const bcrypt = require('bcrypt');
+=======
+<<<<<<< HEAD
+const db = pgp('SQLKORA://postgres:!23Preoccupied@localhost:3001/kora');
+=======
+const db = pgp('SQLKORA://postgres:123@localhost:3001/kora');
+const bcrypt = require('bcrypt');
+>>>>>>> >added some touches
+>>>>>>> >added some touches
 
 module.exports = db;
